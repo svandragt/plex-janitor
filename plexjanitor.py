33884e5d57c42bbd2bd7a6e6430d0c42d6c6e1c0
Sander van Dragt <sander@vandragt.com>
2022-06-26 00:50:26 +0100

    first iteration

---
+++ plexjanitor.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import sys
+import configparser
+from plexapi.myplex import MyPlexAccount
+
+# script action
+if len(sys.argv) >= 2:
+    action = sys.argv[1]
+else:
+    action = "dryrun"
+    print('Note: To delete, add "delete" as the script parameter')
+print("Action:", action)
+
+# read config
+configparser = configparser.RawConfigParser()
+configparser.read(r"config.ini")
+username = configparser.get("Plex", "username")
+password = configparser.get("Plex", "password")
+servername = configparser.get("Plex", "servername")
+
+# sign in
+account = MyPlexAccount(username, password)
+plex = account.resource(servername).connect()  # returns a PlexServer instance
+
+# delete tv shows ep
+print("")
+print("TV")
+items = {}
+for ep in plex.library.section("TV Shows").collection("Deletable TV").items():
+    if ep.show() in items:
+        prev = items[ep.show()]
+        print("deletable", prev)
+        if action == "delete":
+            prev.delete()
+            print("deleted", prev)
+
+    items[ep.show()] = ep
+
+# delete films
+print("")
+print("FILMS")
+for film in plex.library.section("Movies").collection("Deletable Movies").items():
+    print("deletable", film)
+    if action == "delete":
+        film.delete()
+        print("deleted", film)
---
+++ readme.md
@@ -0,0 +1,33 @@
+# PLEX Janitor
+
+Install:
+
+Create `config.ini` with the following contents:
+
+```ini
+[Plex]
+user = "<username>"
+pass = "<password>"
+name = "<plex servername>"
+```
+
+Run the script:
+
+```shell
+poetry install
+poetry run python plexjanitor.py
+```
+
+The script makes the following assumptions:
+
+- There's two sections: "TV Shows" and "Movies"
+- The TV Shows section has a collection "Keep" and a collection "Deletable TV"
+- The Movies section has a collection "Keep" and a collection "Deletable Movies"
+- Add items to the Keep collection manually (for TV Shows tag the show, not the episode). These will not be deleted.
+- The Deletable* collections must have these filters:
+  - Plays is greater than 0
+  - AND Collection is not Keep
+
+This script will then go through each section and list deletable items. For TV Shows the most recent episode will be kept, as a marker for future seasons.
+
+By default deletable items are listed but not deleted. By passing `delete` as a script parameter, items will be actually deleted!
